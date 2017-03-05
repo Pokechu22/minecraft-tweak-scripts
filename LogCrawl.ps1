2586c11a7b6c5cc4b2d510756f5ced0424bc822b
Pokechu22 <Pokechu022@gmail.com>
2017-03-05 14:34:53 -0800

    Add my log crawling script
    This is fairly old, but I needed it and keep forgetting where it is on my drive, so it's better to put it somewhere I can easilly find it.  Also, update readme.

---
+++ LogCrawl.ps1
@@ -0,0 +1,28 @@
+﻿$pathin = Read-Host -Prompt "Path to logs"
+$regex = Read-Host -Prompt "Regex (case-insensitive)"
+
+$path = [System.Environment]::ExpandEnvironmentVariables($pathin)
+echo ("Searching for log files in " + $path)
+
+$files = [System.IO.Directory]::GetFiles($path)
+$lastfile = $null
+foreach ($file in $files) {
+    if (-not $file.EndsWith(".gz")) {
+        continue;
+    }
+    $stream = [System.IO.File]::OpenRead($file)
+    $gzstream = [System.IO.Compression.GZipStream]::new($stream, [System.IO.Compression.CompressionMode]::Decompress)
+    $streamreader = [System.IO.StreamReader]::new($gzstream)
+    while ($line = $streamreader.ReadLine()) {
+        if ($line -imatch $regex) {
+            if ($file -ne $lastfile) {
+                $lastfile = $file
+                echo ("Match(es) in " + $file + ":")
+            }
+            echo $line
+        }
+    }
+    $streamreader.Close()
+    $gzstream.Close()
+    $stream.Close()
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Miscellaneous scripts/tweaks
 
-Scripts and tweaks for Minecraft, mostly used to debug things/display information that's useful for documentation.  These scripts use [Jawa](http://jawa.tkte.ch), and are provided as-is (they usually require additional work to actually be used with the real game, EG modifying version JSONs).
+Scripts and tweaks for Minecraft, mostly used to debug things/display information that's useful for documentation.  These scripts are provided as-is (they usually require additional work to actually be used with the real game, EG modifying version JSONs).
+
+They're written in whatever language I felt like at the time.  Some use python and [Jawa](http://jawa.tkte.ch), others are JARs, and others are in powershell.
